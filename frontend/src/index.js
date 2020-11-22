8469e046535fe4551d1560dde25f1209a9b0f661
brandon-perry <brandonperryofficial@gmail.com>
2020-11-22 14:03:58 -0500

    initial commit with backend and front-end from authentication project

---
+++ frontend/src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+import {Provider} from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
+import configureStore from './store/index'
+
+import {restoreCSRF, fetch} from './store/csrf'
+import * as sessionActions from './store/session'
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = fetch;
+  window.store = store
+  window.sessionActions = sessionActions;
+
+}
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
